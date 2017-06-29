477544d84109e141321a69b1c9779789ec46d12f
Levi Donaldson <lcdbacanas@gmail.com>
2017-06-28 20:33:11 -0500

    Update simple_math.ex

--- simple_math.ex
+++ simple_math.ex
@@ -5,3 +5,11 @@ defmodule Number do
 end
 
 IO.inspect Number.add(5, 2) // should give you 7
+
+defmodule Number do
+  def sub(a, b) do
+    a - b
+  end
+end
+
+IO.inspect Number.sub(5, 2) // should give you 3
