c061efe4d7d70fbbbec129b39c0d8be9cc036fee
Levi Donaldson <lcdbacanas@gmail.com>
2017-07-28 16:13:28 -0500

    Update pattern_match.ex

--- pattern_match.ex
+++ pattern_match.ex
@@ -3,3 +3,12 @@ IO.inspect value
 
 list = {"hey", "there", "r", 2, "d", 2, "I hope", "c", 3, "P O", "made it safely"}
 IO.inspect list
+
+def load(filename) do
+  {status, binary} = File.read(filename)
+
+  case status do
+    :ok -> :erlang.binary_to_term binary
+    :error -> "that file does not exist"  
+  end
+end
