8fddbc413af5cb9706d7eb47963fdceb4cc53784
Levi Donaldson <lcdbacanas@gmail.com>
2017-07-30 14:41:10 -0500

    Update private_functions.ex

--- private_functions.ex
+++ private_functions.ex
@@ -3,3 +3,8 @@
 defp get_geolocation(postal_code) do
   Store.get_geolocation(postal_code)
 end
+
+
+# This is a shorter one
+
+defp format_postal_code(postal_code) when is_binary(postal_code), do: postal_code
