089f55f93d43640a29219cebde4a23af3dab23d8
Levi Donaldson <lcdbacanas@gmail.com>
2017-09-28 20:53:10 -0500

    added parser for http request

--- hello_world/handler.ex
+++ hello_world/handler.ex
@@ -2,5 +2,11 @@ defmodule Handler do
 
   def handle(request) do
   end
+  
+  def parse(request) do
+    # Convert String into a map
+    
+    conv = %{method: "GET", path: "/profile", response_body: "" }
+  end
 
 end
