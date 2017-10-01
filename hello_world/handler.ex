529f1f222125f605da6d7a285cb04c5149ca2e9d
Levi Donaldson <lcdbacanas@gmail.com>
2017-09-30 20:51:17 -0500

    gave supposed request response

--- hello_world/handler.ex
+++ hello_world/handler.ex
@@ -14,5 +14,24 @@ defmodule Handler do
     
     conv = %{ method: "GET", path: "/profile", response_body: "name, age, gender" }
   end
+  
+  def responseFormat (conv) do
+    # Response string created
+  
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 30
+    
+    name, age, gender
+    """
+  end
 
 end
+
+request = """
+    GET /Profile HTTP/1.1
+    Host: fireflytechlabs.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+    """
