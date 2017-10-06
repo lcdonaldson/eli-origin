32baa5287c3b7185e36d8ac0b0ff1e1885dd65bf
Levi Donaldson <lcdbacanas@gmail.com>
2017-10-06 18:57:18 -0500

    Update handler.ex

--- hello_world/handler.ex
+++ hello_world/handler.ex
@@ -3,9 +3,12 @@ defmodule Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> responseFormat
   end
+  
+  def log(conv), do: IO.inspect conv
   
   def parse(request) do
     # Convert String into a map
@@ -17,34 +20,59 @@ defmodule Handler do
         |> String.split(" ")
     %{ method: "GET", path: "/profile", response_body: " " }
   end
-  
+
   def route(conv) do
     # New map that has response body filled.
-    
+
     conv = %{ method: "GET", path: "/profile", response_body: "name, age, gender" }
   end
-  
+
   def responseFormat (conv) do
     # Response string created
-  
+
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
-    
+
     #{conv.resp_body}
     """
   end
-
+  
 end
 
 request = """
-    GET /Profile HTTP/1.1
-    Host: fireflytechlabs.com
-    User-Agent: ExampleBrowser/1.0
-    Accept: */*
-    """
-    
-response = Handler.handle(request)
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
 
 IO.puts response
