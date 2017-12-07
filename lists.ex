556cc94e78b04b37df1aac4a811f1e1802bbba2e
Levi Donaldson <lcdbacanas@gmail.com>
2017-12-06 22:08:33 -0600

    head/tail in lists

--- lists.ex
+++ lists.ex
@@ -14,3 +14,13 @@ lists = [:hello, "world", 42]
 for list <- lists do
   IO.inspect list
 end
+
+# head and tail in lists. hd is the first item in the list. tl is everything after the first item.
+
+x = hd [5, "Banana", :ok]
+IO.inspect x
+# gives you [5]
+
+y = tl [5, "Banana", :ok]
+IO.inspect y
+# gives you ["Banana", :ok]
